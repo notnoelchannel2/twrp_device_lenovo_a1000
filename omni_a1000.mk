d5db39c70bbbeb2a116918d7b1bedb819d8e6b9b
nnc <notnoelchannel1@protonmail.com>
2026-09-07 16:48:48 +0200

    another whoops

--- omni_a1000.mk
+++ omni_a1000.mk
@@ -11,7 +11,7 @@ $(call inherit-product, vendor/omni/config/common.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a1000
 PRODUCT_NAME := omni_a1000
-PRODUCT_BRAND := samsung
+PRODUCT_BRAND := lenovo
 PRODUCT_MODEL := Lenovo A1000
-PRODUCT_MANUFACTURER := samsung
+PRODUCT_MANUFACTURER := lenovo
 PRODUCT_CHARACTERISTICS := phone
