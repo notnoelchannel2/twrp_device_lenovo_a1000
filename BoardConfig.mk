dc7ccc5dd7126dc0c80e5388c6e78ba91e553992
nnc <notnoelchannel1@protonmail.com>
2026-09-08 12:27:38 +0200

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,7 +43,7 @@ BOARD_HAS_LARGE_FILESYSTEM := true
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := console=ttyS1,115200n8 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := console=ttyS1,115200n8
 BOARD_KERNEL_PAGESIZE := 2048
 TARGET_PREBUILT_KERNEL := device/lenovo/a1000/prebuilt/zImage
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/lenovo/a1000/prebuilt/dtb
